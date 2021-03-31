38546151b8f5a72021e5b959117641b631153874
h4zzkR <tramvenus@yandex.ru>
2021-03-31 13:48:17 +0300

    added task

--- push.sh
+++ push.sh
@@ -3,5 +3,6 @@ branch_name=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 #echo $branch_name
 git add .
 git commit -m "added task"
-git push origin $BRANCH
+#git push origin $BRANCH
+git push --set-upstream origin $BRANCH
 echo pushed
